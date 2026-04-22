79c9165b92b979615b7c75fae60ea7f14f05524b
kasakmahra23-ux <kasakmahra23@gmail.com>
2026-04-23 02:51:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,34 @@
-import os
-
-# Download model from Google Drive (only once)
-if not os.path.exists("final_plant_model.keras"):
-    url = "https://drive.google.com/uc?id=1_O0vosEt-AHD5_fEGFnBABgafb7WXV39"
-    gdown.download(url, "final_plant_model.keras", quiet=False)
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
+import gdown
+import os
+
+# 🔹 Download model from Google Drive
+url = "https://drive.google.com/uc?id=1_O0vosEt-AHD5_fEGFnBABgafb7WXV39"
+output = "final_plant_model.h5"
+
+if not os.path.exists(output):
+    gdown.download(url, output, quiet=False)
+
+# 🔹 Load model
+model = load_model("final_plant_model.h5", compile=False)
 import json
 
+with open("class_names.json") as f:
+    class_names = list(json.load(f).keys())
 app = Flask(__name__)
-
-# Load model
-model = load_model('final_plant_model.keras')
-
-# Load class names
-with open('class_names.json') as f:
-    class_indices = json.load(f)
-
-class_names = list(class_indices.keys())
 
 @app.route('/')
 def home():
-    return "🌿 Plant Disease API Live!"
+    return "🌿 Plant Disease API Running!"
 
 @app.route('/predict', methods=['POST'])
 def predict():
     file = request.files['file']
     
-    filepath = 'temp.jpg'
+    filepath = "leaf.webp"
     file.save(filepath)
 
     img = image.load_img(filepath, target_size=(128,128))
@@ -39,11 +38,15 @@ def predict():
     prediction = model.predict(img_array)
     index = np.argmax(prediction)
 
+    result = class_names[index]
+    confidence = float(prediction[0][index]) * 100
+
     return jsonify({
-        "disease": class_names[index],
-        "confidence": float(prediction[0][index]) * 100
+        "disease": result,
+        "confidence": round(confidence, 2)
     })
 
+# 🔹 Required for Render
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
